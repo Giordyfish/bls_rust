b78d8db8125f6ea7889612b250bcf2aa43577964
Giordyfish <giordano.pescetelli@gmail.com>
2022-05-27 17:12:15 +0200

    test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+// use bls::{Serialize, PrivateKey};
+// use bls_signatures as bls;
+use bls;
+
+
+
 fn main() {
-    println!("Hello, world!");
+    
+    // // TEST private key gen
+    // let pvkey = bls::PrivateKey::new("askdlmaksmcokijqewnaonsjcfbwasoinciasujbceiaocbeocnawi");
+    // println!("{:?}", pvkey);
+    
+    // // TEST private key to bytes
+    // let key = pvkey.as_bytes();
+    // println!("{:?}", key);
+
+    // // TEST private key from bytes
+    // let keyslice = key.as_slice();
+    // let pvkey2 : PrivateKey = Serialize::from_bytes(keyslice).unwrap();
+    // println!("{:?}", pvkey2);
+    
+    // assert_eq!(pvkey, pvkey2);
+
+    // // ############################
+
+    // // TEST public key
+
 }
